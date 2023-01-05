19262c64bf958b937aa38641015adfd17dc18f55
Ian <ianmcm.89@gmail.com>
2023-01-05 12:40:49 -0500

    add layout to project-1

--- pages/projects/project-1.js
+++ pages/projects/project-1.js
@@ -1,3 +1,17 @@
-export default function FirstPost() {
-  return <h1>First Project</h1>;
+import Head from 'next/head';
+import Link from 'next/link';
+import Layout from '../../components/layout';
+
+export default function FirstProject() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Project</title>
+      </Head>
+      <h1>First Project</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </Layout>
+  )
 }
